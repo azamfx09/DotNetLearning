bd09561d10b811e7e89950561b1cf162c88c8892
azamfx09 <dd1778673@gmail.com>
2026-01-21 13:46:40 +0500

    add

--- Module-01-Basics/Lesson-01-Console-Output/ConsoleOutputApp/ConsoleOutputApp/Program.cs
+++ Module-01-Basics/Lesson-01-Console-Output/ConsoleOutputApp/ConsoleOutputApp/Program.cs
@@ -8,7 +8,8 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("I am learning C# step by step");
             Console.WriteLine("My favourite hobby Coding");
-            Console.WriteLine("My name is Azamat");
+            Console.WriteLine("My name is Azamat");\
+
         }
     }
 }
